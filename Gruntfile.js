c9065ae2435c44656b76e756cfaf979676ec7e7d
Eric Kryski <e.kryski@gmail.com>
2014-07-20 00:15:58 -0600

    switching up test reporter to spec

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function (grunt) {
       test: 'test/**/*.js'
     },
     simplemocha: {
+      options: {
+        reporter: 'spec'
+      },
       all: {
         src: ['test/**/*.test.js']
       }
