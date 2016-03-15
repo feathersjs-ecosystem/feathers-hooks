4e0c038c3d9dd4e6e58be09f7fbb96fbd9b5a7d3
David Luecke <daff@neyeon.com>
2016-03-14 23:29:01 -0700

    Adding functionality and tests for the populate hook

--- src/bundled.js
+++ src/bundled.js
@@ -94,3 +94,66 @@ export function disable(realm, ... args) {
     };
   }
 }
+
+export function populate(target, options) {
+  options = Object.assign({}, options);
+
+  if (!options.service) {
+    throw new Error('You need to provide a service');
+  }
+
+  const field = options.field || target;
+
+  return function(hook) {
+    function populate(item) {
+      if(!item[field]) {
+        return Promise.resolve(item);
+      }
+
+      // Find by the field value by default or a custom query
+      const id = item[field];
+
+      // If it's a mongoose model then
+      if (typeof item.toObject === 'function') {
+        item = item.toObject(options);
+      }
+      // If it's a Sequelize model
+      else if (typeof item.toJSON === 'function') {
+        item = item.toJSON(options);
+      }
+
+      return hook.app.service(options.service)
+        .get(id, hook.params)
+        .then(relatedItem => {
+          if(relatedItem) {
+            item[target] = relatedItem;
+          }
+
+          return item;
+        }).catch(() => item);
+    }
+
+    if(hook.type === 'after') {
+      const isPaginated = (hook.method === 'find' && hook.result.data);
+      const data = isPaginated ? hook.result.data : hook.result;
+
+      if (Array.isArray(data)) {
+        return Promise.all(data.map(populate)).then(results => {
+          if(isPaginated) {
+            hook.result.data = results;
+          } else {
+            hook.result = results;
+          }
+
+          return hook;
+        });
+      }
+
+      // Handle single objects.
+      return populate(hook.result).then(item => {
+        hook.result = item;
+        return hook;
+      });
+    }
+  };
+}
