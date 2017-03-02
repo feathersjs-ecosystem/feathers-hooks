523ac39827b4e30565fe578366a291ac56c3f6e2
Richard Michael Coo <augustuslogos@gmail.com>
2017-03-02 10:29:20 +0800

    Additional hook typedefs (#139)
    * Augment Service<T> typedef with before() and after() methods
    
    * Place custom types and interfaces under namespace

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,37 @@
+import * as feathers from 'feathers';
 
+declare module 'feathers' {
+  interface Service<T> {
+    before(hooks: hooks.HookMap);
+    after(hooks: hooks.HookMap);
+  }
+}
 
-declare function configure(): () =>void;
-declare namespace configure{}
-export = configure;
+declare function hooks(): () => void;
+
+declare namespace hooks {
+  interface Hook {
+    <T>(hook: HookProps<T>): Promise<any> | void;
+  }
+
+  interface HookProps<T> {
+    method?: string;
+    type: 'before' | 'after';
+    params?: any;
+    data?: T;
+    result?: T;
+    app?: feathers.Application;
+  }
+
+  interface HookMap {
+    all?: Hook | Hook[];
+    find?: Hook | Hook[];
+    get?: Hook | Hook[];
+    create?: Hook | Hook[];
+    update?: Hook | Hook[];
+    patch?: Hook | Hook[];
+    remove?: Hook | Hook[];
+  }
+}
+
+export = hooks;
